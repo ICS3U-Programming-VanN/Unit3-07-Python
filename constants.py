5fe4e9a068d5cb6836e9cea691f6a8144a35bbbf
Van Nguyen <van.nguyen2@stu.ocsb.ca>
2022-10-19 18:16:07 +0000

    Changed minor spacing.

--- constants.py
+++ constants.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
- 
+
 # Created by: Van Nguyen
 # Created on: October 19, 2022
 # This file contains constants (age range for dating)
- 
+
 # Declared Constants
 MINIMUM_AGE_THRESHOLD = 25
 MAXIMUM_AGE_THRESHOLD = 40
- 
